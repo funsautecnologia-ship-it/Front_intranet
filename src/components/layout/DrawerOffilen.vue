fd16b57935f876f973c22b68d2ad09bbb53af910
funsautecnologia-ship-it <funsautecnologia@gmail.com>
2025-12-22 14:21:34 -0400

    ajustando layout

--- src/components/layout/DrawerOffilen.vue
+++ src/components/layout/DrawerOffilen.vue
@@ -1,56 +1,63 @@
 <template>
     <v-main>
         <v-app-bar color="#009688" dark>
-        <!-- <v-app-bar> -->
-            <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer">
+            <v-app-bar-nav-icon 
+                variant="text" 
+                @click.stop="drawer = !drawer"
+                class="d-md-none"
+            >
             </v-app-bar-nav-icon>
-            <v-app-bar-title> Bem vindo a intranet Hospital Regional de Nova Andradina Versão Beta</v-app-bar-title>
+            
+            <v-app-bar-title class="text-subtitle-2 text-md-subtitle-1">Bem vindo a intranet Hospital Regional de Nova Andradina Versão Beta</v-app-bar-title>
+            
             <v-spacer></v-spacer>
-            <v-btn>
+
+            <!-- Menu Desktop -->
+            <div class="d-none d-md-flex align-center">
+                <v-btn
+                    v-for="item in offlineItems"
+                    :key="item.title"
+                    variant="text"
+                    @click="navigate(item)"
+                    class="mx-1 text-white"
+                >
+                    <v-icon start>{{ item.icon }}</v-icon>
+                    {{ item.title }}
+                </v-btn>
+            </div>
+
+            <v-btn class="ml-2">
                 <v-avatar>
                     <v-img src="../../../public/logoH.png" alt="Logo" />
                 </v-avatar>
             </v-btn>
-            
         </v-app-bar>
-      
 
-
-            <v-navigation-drawer
-                v-model="drawer"
-                app
-                temporary
-                :location="$vuetify?.display?.mobile ? 'bottom' : undefined"
-                >
-                <v-list>
+        <v-navigation-drawer
+            v-model="drawer"
+            app
+            temporary
+            :location="$vuetify?.display?.mobile ? 'bottom' : undefined"
+        >
+            <v-list>
                 <v-list-item>
                     <v-list-item-title>Modo Offline</v-list-item-title>
                 </v-list-item>
                 <v-divider></v-divider>
                 <v-list-item v-for="item in offlineItems" :key="item.title" router @click="navigate(item)">
-                    
-                    <v-icon>{{ item.icon }}</v-icon>
-                   
+                    <template v-slot:prepend>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </template>
                     <v-list-item-title>{{ item.title }}</v-list-item-title>
                 </v-list-item>
-                </v-list>
-            </v-navigation-drawer>
+            </v-list>
+        </v-navigation-drawer>
         
-        
-             <v-container>
-                <router-view  />
-                <AppFooter />
-                
-            </v-container>
-
-       
-       
-    
+        <v-container>
+            <router-view  />
+            <AppFooter />
+        </v-container>
     </v-main>
-    
-
-  
-  
 </template>
 
 <script setup>
